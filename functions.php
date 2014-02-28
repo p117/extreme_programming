f987a91f3fc0eba9c242e6fa48e6ef13eedd643a
Ulrich <ulrich.palou@gmail.com>
2014-02-28 15:09:17 +0100

    ajout fonction

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+function countWords($file)
+{
+	$rows = "";
+	try
+	{
+		$handle = fopen($file,"r");
+	}
+	catch(Exception $e)
+	{
+		echo 'file doesn\'t exist';
+		exit;
+	}
+	while(!feof($handle))
+	{
+		$rows .= fgets($handle);
+	}
+	return countWordsByRow($rows);
+}
+
+function countWordsByRow($string)
+{
+	/*$string = str_replace("'"," ",$string);
+	$string = str_replace("?"," ",$string);
+	$string = str_replace("."," ",$string);
+	$string = str_replace("!"," ",$string);
+	$string = str_replace(","," ",$string);*/
+
+	$string = preg_replace("/(('+)|(-+)|(\?+)|(!+)|(\.+)|(,+)|\\n)/"," ",$string);
+	$string = preg_replace("/\s+/"," ",$string);
+	trim($string);
+	$array = explode(" ",$string);
+	return count($array);
+}
+
+?>
