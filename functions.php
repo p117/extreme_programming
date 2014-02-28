627919ac2e57124113a0ddbcb3aec6f680cf013c
Ulrich <ulrich.palou@gmail.com>
2014-02-28 16:02:38 +0100

    remove accent 3

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
-function word_remove_accents($str, $charset='utf-8')
+function word_remove_accents($string, $charset='utf-8')
 {
-    $string = htmlentities($str, ENT_NOQUOTES, $charset);
-    $string = preg_replace('#&([A-za-z])(?:acute|cedil|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
-    $string = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
-    return $str;
+    $string = htmlentities($string, ENT_NOQUOTES, $charset);
+    $string = preg_replace('#&([A-za-z])(?:acute|cedil|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $string);
+    $string = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $string);
+    return $string;
 }
 function countWordsByRow($string)
 {
