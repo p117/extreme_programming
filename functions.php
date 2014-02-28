21f8db1cb210b9f42d5b4110454c46e4722567fd
unknown <pierre.lecigne@cifacom.com>
2014-02-28 15:54:15 +0100

    changements

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function countWordsByRow($string)
 {
-	$string = preg_replace("#[^a-zA-Z]#", " ", $string);
+	$string = preg_replace("#[^0-9a-zA-Z]#", " ", $string);
 	$string = preg_replace("/\s+/"," ",$string);
 	$string = trim($string);
 	$array = explode(" ",$string);
